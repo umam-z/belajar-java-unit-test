e3d999546cd2430587134a6d2562a61bbf1362a8
ZuperProgrammer <sirajul.umam6@gmail.com>
2024-08-25 10:18:48 +0700

    Belajar Unit Test

--- src/test/java/zuper/programmer/test/CalculatorTest.java
+++ src/test/java/zuper/programmer/test/CalculatorTest.java
@@ -1,13 +1,39 @@
 package zuper.programmer.test;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import zuper.programmer.test.generator.SimpleDisplayNameGenerator;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+//@DisplayName("test untuk calculator class")
+@DisplayNameGeneration(SimpleDisplayNameGenerator.class)
+//@DisplayNameGeneration(DisplayNameGenerator.Standard.class)
 public class CalculatorTest {
 
     Calculator calculator = new Calculator();
 
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before All");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("Before All");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        System.out.println("Before Each");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("After Each");
+    }
+
     @Test
+//    @DisplayName("test skanario suksess untuk method add(Integer, Integer)")
     public void testAddSuccess() {
         var result = calculator.add(10, 10);
         assertEquals(20, result);
@@ -27,5 +53,11 @@ public class CalculatorTest {
         assertThrows(IllegalArgumentException.class, () -> {
             calculator.devide(10, 0);
         });
+    }
+
+    @Test
+    @Disabled
+    public void testComingSoon() {
+
     }
 }
---
+++ src/test/java/zuper/programmer/test/generator/SimpleDisplayNameGenerator.java
@@ -0,0 +1,23 @@
+package zuper.programmer.test.generator;
+
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+import java.lang.reflect.Method;
+
+public class SimpleDisplayNameGenerator implements DisplayNameGenerator {
+    @Override
+    public String generateDisplayNameForClass(Class<?> testClass) {
+        return "Test " + testClass.getSimpleName();
+    }
+
+    @Override
+    public String generateDisplayNameForMethod(Class<?> testClass, Method testMethod) {
+        return "Test " + testClass.getSimpleName() + "." + testMethod.getName();
+    }
+
+    @Override
+    public String generateDisplayNameForNestedClass(Class<?> nestedClass) {
+        return "";
+    }
+
+}
